712e662cbf1ad208eb3137f5dae1c2e85cb2f406
El Haroria Abdel-Hamid <abdel-hamid.el-haroria@etu.iut-tlse3.fr>
2024-11-15 15:14:12 +0100

    t

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -3,22 +3,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static  String url = "jdbc:mysql://localhost:3306/gestion_bien_immobilier"; 
-    private static String utilisateur = "root";
-    private static String mdp = ""; 
+
+    // Définir les constantes pour la connexion
+    private static final String url = "jdbc:mysql://mysql-projetr301.alwaysdata.net/projetr301_gestion_immobilier";
+    private static final String utilisateur = "384907";
+    private static final String mdp = "rootiutinfo";
 
     public static Connection connect() {
         try {
-            Connection conn = DriverManager.getConnection(url, utilisateur,mdp );
+            // Etablir une connexion à la base de données
+            Connection conn = DriverManager.getConnection(url, utilisateur, mdp);
             System.out.println("Connexion réussie !");
             return conn;
         } catch (SQLException e) {
-            System.out.println("Erreur de connexion : " + e.getMessage());
+            // Gérer les exceptions et afficher un message d'erreur
+            System.err.println("Erreur lors de la connexion à la base de données : " + e.getMessage());
             return null;
         }
     }
 
     public static void main(String[] args) {
+        // Tester la connexion
         connect();
     }
 }

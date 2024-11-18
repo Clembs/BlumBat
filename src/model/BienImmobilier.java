b16aa7cae16f5a60db785027feaa6890e0ae2a3c
Clément Voisin <clement.voisin@etu.iut-tlse3.fr>
2024-11-18 12:05:24 +0100

    getters et évaluateurs bien immobilier, associations avec locations

--- src/model/BienImmobilier.java
+++ src/model/BienImmobilier.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.List;
+
 public class BienImmobilier {
   private String id;
   private TypeBien typeBien;
@@ -9,6 +11,7 @@ public class BienImmobilier {
   private String nomResidence;
   private int codePostal;
   private String ville;
+  private List<Location> locations;
 
   public BienImmobilier(String id, TypeBien typeBien, String adresse, String complementAdresse, int numeroAppt,
       String nomResidence, int codePostal, String ville) {
@@ -20,5 +23,49 @@ public class BienImmobilier {
     this.nomResidence = nomResidence;
     this.codePostal = codePostal;
     this.ville = ville;
+  }
+
+  public String getId() {
+    return this.id;
+  }
+
+  public TypeBien getTypeBien() {
+    return this.typeBien;
+  }
+
+  public String getAdresse() {
+    return this.adresse;
+  }
+
+  public String getComplementAdresse() {
+    return this.complementAdresse;
+  }
+
+  public int getNumeroAppt() {
+    return this.numeroAppt;
+  }
+
+  public String getNomResidence() {
+    return this.nomResidence;
+  }
+
+  public int getCodePostal() {
+    return this.codePostal;
+  }
+
+  public String getVille() {
+    return this.ville;
+  }
+
+  public List<Location> getLocations() {
+    return this.locations;
+  }
+
+  public void addLocation(Location location) {
+    this.locations.add(location);
+  }
+
+  public void removeLocation(Location location) {
+    this.locations.remove(location);
   }
 }

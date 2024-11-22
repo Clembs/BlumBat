48ab06b28417f06bef5421b48d9cc35a2c6bdf11
Clément Voisin <clement.voisin@etu.iut-tlse3.fr>
2024-11-22 09:57:58 +0100

    màj emplacements class & package view sur les vues

--- src/view/FEN_CONNEXION_CLIENT.java
+++ src/view/FEN_CONNEXION_CLIENT.java
@@ -1,16 +1,39 @@
-import java.awt.EventQueue;
+package view;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.BorderLayout;
-import javax.swing.JLabel;
-import java.awt.FlowLayout;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.*;
 
-public class FEN_IDENTIFICATION_CLIENT extends JFrame {
+public class FEN_CONNEXION_CLIENT extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+
+	public FEN_CONNEXION_CLIENT() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+
+		JLabel lblNewLabel = new JLabel("New label");
+		panel.add(lblNewLabel);
+
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		panel_1.add(lblNewLabel_1);
+
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		panel_1.add(lblNewLabel_2);
+	}
 
 	/**
 	 * Launch the application.
@@ -19,42 +42,13 @@ public class FEN_IDENTIFICATION_CLIENT extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					FEN_IDENTIFICATION_CLIENT frame = new FEN_IDENTIFICATION_CLIENT();
+					FEN_CONNEXION_CLIENT frame = new FEN_CONNEXION_CLIENT();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public FEN_IDENTIFICATION_CLIENT() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.NORTH);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		panel.add(lblNewLabel);
-		
-		JPanel panel_1 = new JPanel();
-		contentPane.add(panel_1, BorderLayout.CENTER);
-		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		panel_1.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		panel_1.add(lblNewLabel_2);
 	}
 
 }

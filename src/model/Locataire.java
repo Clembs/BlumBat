cd55fd9a8f316522343833c415309a761cacf2c0
Kylian Pawilowski <kylian.pawilowski@etu.iut-tlse3.fr>
2024-11-22 14:29:29 +0100

    Ajout dao au locataire et modification class locataire

--- src/model/Locataire.java
+++ src/model/Locataire.java
@@ -1,22 +1,18 @@
 package model;
 
-import java.util.List;
-
 public class Locataire {
   private String id;
   private String nom;
   private String prenom;
   private String email;
   private String telephone;
-  private List<Location> locations;
 
-  public Locataire(String id, String nom, String prenom, String email, String telephone, List<Location> locations) {
+  public Locataire(String id, String nom, String prenom, String email, String telephone) {
     this.id = id;
     this.nom = nom;
     this.prenom = prenom;
     this.email = email;
     this.telephone = telephone;
-    this.locations = locations;
   }
 
   public String getId() {
@@ -39,15 +35,4 @@ public class Locataire {
     return telephone;
   }
 
-  public List<Location> getLocations() {
-    return this.locations;
-  }
-
-  public void addLocation(Location location) {
-    this.locations.add(location);
-  }
-
-  public void removeLocation(Location location) {
-    this.locations.remove(location);
-  }
 }

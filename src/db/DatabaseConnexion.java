4da2b9662f4e41de4a4d5d63bf4e08fad916c211
Clément Voisin <clement.voisin@etu.iut-tlse3.fr>
2024-11-22 08:36:46 +0100

    corrections à getConnexion : typo, pas de moyen de récupérer la connexion, etc, package

--- src/db/DatabaseConnexion.java
+++ src/db/DatabaseConnexion.java
@@ -1,26 +1,31 @@
+package db;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConnection {
+public class DatabaseConnexion {
 
     private static final String url = "jdbc:mysql://mysql-projetr301.alwaysdata.net/projetr301_gestion_immobilier";
     private static final String utilisateur = "384907";
     private static final String mdp = "rootiutinfo";
+    private Connection connection;
+    private static DatabaseConnexion instance;
 
-    public static Connection connect() {
+    public static synchronized Connection getConnexion() {
+        if (instance == null) {
+            instance = new DatabaseConnexion();
+        }
+        return instance.connection;
+    }
+
+    public DatabaseConnexion() {
         try {
-            Connection conn = DriverManager.getConnection(url, utilisateur, mdp);
+            this.connection = DriverManager.getConnection(url, utilisateur, mdp);
             System.out.println("Connexion réussie !");
-            return conn;
         } catch (SQLException e) {
-          
             System.err.println("Erreur lors de la connexion à la base de données : " + e.getMessage());
-            return null;
         }
     }
 
-    public static void main(String[] args) {
-        connect();
-    }
 }

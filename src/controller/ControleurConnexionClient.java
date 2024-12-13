869fa3426c81cade60f62e54810d4cb23848ce16
Clément Voisin <clement.voisin@etu.iut-tlse3.fr>
2024-12-13 01:27:51 +0100

    feat: montrer la fenêtre des biens une fois connecté

--- src/controller/ControleurConnexionClient.java
+++ src/controller/ControleurConnexionClient.java
@@ -6,35 +6,44 @@ import javax.swing.*;
 
 import dao.ProprietaireDAO;
 import model.Proprietaire;
+import view.FenBiens;
 import view.FenConnexionClient;
 
 public class ControleurConnexionClient implements ActionListener {
   private FenConnexionClient fenetre;
+  private ProprietaireDAO proprietaireDAO;
 
   public ControleurConnexionClient(FenConnexionClient fenetre) {
     this.fenetre = fenetre;
+    this.proprietaireDAO = new ProprietaireDAO();
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
-    JButton bouton = (JButton) e.getSource();
-
     String email = fenetre.getEmail();
     String motDePasse = fenetre.getMotDePasse();
 
-    ProprietaireDAO daoProprietaire = new ProprietaireDAO();
-
-    System.out.println(email + " " + motDePasse);
-
     try {
-      Proprietaire proprietaire = daoProprietaire.read(email, motDePasse);
+      Proprietaire proprietaire = proprietaireDAO.read(email, motDePasse);
 
-      // TODO: afficher le tableau de bord
-      JOptionPane.showMessageDialog(this.fenetre,
-          String.format("Vous êtes bien connecté %s %s", proprietaire.getPrenom(), proprietaire.getNom()));
-      // this.fenetre.dispose();
+      JOptionPane.showMessageDialog(
+          this.fenetre,
+          String.format("Heureux de vous retrouver, %s %s", proprietaire.getPrenom(), proprietaire.getNom()),
+          "Connection établie",
+          JOptionPane.INFORMATION_MESSAGE);
+
+      // TODO: Montrer le tableau de bord au lieu des biens lorsque ce premier sera
+      // implémenté
+      FenBiens fenetreBiens = new FenBiens(proprietaire);
+      fenetreBiens.setVisible(true);
+      this.fenetre.dispose();
+
     } catch (RuntimeException exception) {
-      JOptionPane.showMessageDialog(this.fenetre, "Aucun compte trouvé avec ces identifiants");
+      JOptionPane.showMessageDialog(
+          this.fenetre,
+          "Aucun compte trouvé avec ces identifiants. Veuillez réessayer.",
+          "Identifiants incorrects",
+          JOptionPane.ERROR_MESSAGE);
     }
 
   }

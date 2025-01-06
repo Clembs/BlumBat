4e84363d06cddb3c518467311445493455411a43
Clément Voisin <clembs@clembs.com>
2025-01-06 18:13:12 +0100

    feat: check des erreurs et ajout des locations pour chaque locataire

--- src/controller/ControleurAjoutLocation.java
+++ src/controller/ControleurAjoutLocation.java
@@ -2,18 +2,25 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 import dao.LocationDAO;
 import model.BienImmobilier;
+import model.Locataire;
+import model.Location;
 import model.Proprietaire;
 import view.FenLocataires;
 import view.FenAjoutLocation;
 
 public class ControleurAjoutLocation implements ActionListener {
     private FenAjoutLocation fenetre;
-    private LocationDAO LocationDAO;
+    private LocationDAO locationDAO;
     private BienImmobilier bien;
     private Proprietaire proprietaire;
 
@@ -21,7 +28,7 @@ public class ControleurAjoutLocation implements ActionListener {
         this.bien = bien;
         this.fenetre = fenetre;
         this.proprietaire = prop;
-        this.LocationDAO = new LocationDAO();
+        this.locationDAO = new LocationDAO();
     }
 
     @Override
@@ -30,27 +37,65 @@ public class ControleurAjoutLocation implements ActionListener {
 
         // Vérifier si le bouton "Louer" est cliqué
         if (boutonClique.getText().equals("Louer")) {
-            // Récupérer les données depuis les champs de texte via les getters
-            // this.loyer = fenetre.getLoyer();
-            // try {
-            // this.dateEntree = fenetre.getDateEntree();
-            // } catch (ParseException error) {
-            // }
-            // try {
-            // this.dateSortie = fenetre.getDateSortie();
-            // } catch (ParseException error) {
-            // }
-            // this.locataire = fenetre.getLocataire();
+            // nettoyage préalable des erreurs
+            this.fenetre.clearErreurs();
 
-            // Location location = new Location(this.loyer, this.dateEntree,
-            // this.dateSortie, this.bien, this.locataire);
-            // LocationDAO.create(location);
+            double loyer = this.fenetre.getLoyer();
+            String rawDateEntree = this.fenetre.getDateEntree();
+            String rawDateSortie = this.fenetre.getDateSortie();
+            LocalDate dateEntree = null;
+            LocalDate dateSortie = null;
 
-            // Afficher un message de confirmation
-            // JOptionPane.showMessageDialog(this.fenetre, "Location ajouté avec succès!");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            if (loyer <= 0) {
+                this.fenetre.addErreur("Le loyer doit être supérieur à 0");
+            }
+
+            try {
+                dateEntree = LocalDate.parse(rawDateEntree, formatter);
+            } catch (DateTimeParseException error) {
+                this.fenetre.addErreur("Date d'entrée invalide. Utilisez le format dd/MM/yyyy.");
+            }
+
+            if (!rawDateSortie.isEmpty()) {
+                try {
+                    dateSortie = LocalDate.parse(rawDateSortie, formatter);
+                } catch (DateTimeParseException error) {
+                    this.fenetre.addErreur("Date de sortie invalide. Utilisez le format dd/MM/yyyy.");
+                }
+
+                if (dateEntree.isAfter(dateSortie)) {
+                    this.fenetre.addErreur("La date de sortie doit être après la date d'entrée");
+                }
+            }
+
+            List<Locataire> locataires = this.fenetre.getLocataires();
+
+            if (locataires == null || locataires.size() == 0) {
+                this.fenetre.addErreur("Veuillez sélectionner au moins un locataire");
+            }
+
+            if (this.fenetre.hasErreurs()) {
+                return;
+            }
+
+            // Pour chaque locataire sélectionné, créer le locataire & la location associée
+            for (Locataire locataire : locataires) {
+                Location location = new Location(
+                        loyer,
+                        dateEntree,
+                        dateSortie,
+                        this.bien,
+                        locataire);
+
+                locationDAO.create(location);
+            }
+
+            JOptionPane.showMessageDialog(this.fenetre, "Location ajouté avec succès!");
 
             // Fermer la fenêtre après ajout
-            // this.fenetre.dispose();
+            this.fenetre.dispose();
 
         } else if (boutonClique.getText().equals("Sélectionner")) {
             FenLocataires nouvelleFenetre = new FenLocataires(proprietaire, fenetre);

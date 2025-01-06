214bd0b81b813c15665883720dada7b67ae6a8e8
Clément Voisin <clembs@clembs.com>
2025-01-06 13:43:17 +0100

    fix: ajout de toute la validation

--- src/controller/ControleurModificationBien.java
+++ src/controller/ControleurModificationBien.java
@@ -4,8 +4,8 @@ import dao.BienDAO;
 import model.BienImmobilier;
 import model.BienLocatif;
 import model.Proprietaire;
-import model.TypeBien;
 import view.FenBiens;
+import view.PanelConsultationBien;
 import view.PanelModificationBien;
 
 import javax.swing.*;
@@ -13,59 +13,90 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ControleurModificationBien implements ActionListener {
-    private FenBiens fenetre;
-    private PanelModificationBien panel;
-    private BienImmobilier bien;
-    private Proprietaire proprietaire;
-    private BienDAO bienDAO;
+  private FenBiens fenetre;
+  private PanelModificationBien panel;
+  private BienImmobilier bien;
+  private Proprietaire proprietaire;
+  private BienDAO bienDAO;
 
-    public ControleurModificationBien(FenBiens fenetre, PanelModificationBien panel, Proprietaire proprietaire,
-            BienImmobilier bien) {
-        this.fenetre = fenetre;
-        this.panel = panel;
-        this.proprietaire = proprietaire;
-        this.bien = bien;
-        this.bienDAO = new BienDAO();
-    }
+  public ControleurModificationBien(FenBiens fenetre, PanelModificationBien panel, Proprietaire proprietaire,
+      BienImmobilier bien) {
+    this.fenetre = fenetre;
+    this.panel = panel;
+    this.proprietaire = proprietaire;
+    this.bien = bien;
+    this.bienDAO = new BienDAO();
+  }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        JButton boutonClique = (JButton) e.getSource();
-        String boutonTexte = boutonClique.getText();
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    JButton boutonClique = (JButton) e.getSource();
+    String boutonTexte = boutonClique.getText();
 
-        if (boutonTexte.equals("Enregistrer")) {
-            panel.clearErreurs();
+    switch (boutonTexte) {
+      case "Enregistrer": {
+        panel.clearErreurs();
 
-            // Récupérer les données des champs
-            String idBien = panel.getIdBien();
-            String adresse = panel.getAdresse();
-            String complementAdresse = panel.getComplementAdresse();
-            String ville = panel.getVille();
-            String codePostal = panel.getCodePostal();
-            TypeBien typeBien = panel.getTypeBien();
-            float surface = panel.getSurface();
-            String nbFiscal = panel.getNbFiscal();
-            int nbPiece = panel.getNbPiece();
+        // Récupérer les données des champs
+        String adresse = panel.getAdresse();
+        String complementAdresse = panel.getComplementAdresse();
+        String ville = panel.getVille();
+        String codePostal = panel.getCodePostal();
+        float surface = panel.getSurface();
+        String nbFiscal = panel.getNbFiscal();
+        int nbPieces = panel.getNbPieces();
 
-            boolean valide = true;
-
-            // Vérification des champs vides
-            if (idBien.isEmpty() || adresse.isEmpty() || ville.isEmpty() || codePostal.isEmpty() ||
-                    surface == 0 || nbFiscal.isEmpty() || nbPiece == 0) {
-                valide = false;
-                panel.addErreur("Veuillez remplir tous les champs.");
-            }
-
-            if (valide) {
-                BienLocatif bien = new BienLocatif(idBien, typeBien, adresse, complementAdresse, codePostal, ville,
-                        nbFiscal, surface, nbPiece);
-                bienDAO.modifierBienDao(bien);
-                System.out.println(bien);
-                panel.clearErreurs();
-                JOptionPane.showMessageDialog(panel, "Votre bien a été modifié avec succès.");
-            }
-
-            panel.closePanel();
+        // Vérification des champs vides
+        if (adresse.isEmpty()) {
+          panel.addErreur("L'adresse ne peut pas être vide.");
         }
+
+        if (ville.isEmpty()) {
+          panel.addErreur("La ville ne peut pas être vide.");
+        }
+
+        if (!codePostal.matches("\\d{5}") /* expression régulière pour 5 chiffres */) {
+          panel.addErreur("Le code postal doit être composé de 5 chiffres.");
+        }
+
+        if (this.bien instanceof BienLocatif) {
+          if (surface <= 0) {
+            panel.addErreur("La surface doit être supérieure à 0m².");
+          }
+
+          if (nbPieces <= 0) {
+            panel.addErreur("Le nombre de pièces doit être supérieur à 0.");
+          }
+
+          if (!nbFiscal.matches("\\d{12}") /* même chose pour 12 chiffres */) {
+            panel.addErreur("Le numéro fiscal doit être composé de 12 chiffres.");
+          }
+        }
+
+        if (panel.hasErreurs()) {
+          return;
+        }
+
+        BienLocatif nouveauBien = new BienLocatif(this.bien.getId(), this.bien.getTypeBien(), adresse,
+            complementAdresse,
+            codePostal, ville,
+            nbFiscal, surface, nbPieces);
+
+        bienDAO.update(nouveauBien);
+
+        JOptionPane.showMessageDialog(panel, "Votre bien a été modifié avec succès.");
+
+        PanelConsultationBien panelConsultation = new PanelConsultationBien(fenetre, proprietaire, nouveauBien);
+
+        fenetre.setPanelCentral(panelConsultation);
+        break;
+      }
+      case "Annuler": {
+        PanelConsultationBien panelConsultation = new PanelConsultationBien(fenetre, proprietaire, bien);
+
+        fenetre.setPanelCentral(panelConsultation);
+        break;
+      }
     }
+  }
 }

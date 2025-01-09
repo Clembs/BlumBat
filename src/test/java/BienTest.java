be02cc28a56fd59a0bb04e3d3560a63326426017
Clément Voisin <clembs@clembs.com>
2025-01-09 14:33:47 +0100

    fix: suppression du bien après des tests individuels

--- src/test/java/BienTest.java
+++ src/test/java/BienTest.java
@@ -16,163 +16,148 @@ import model.BienImmobilier;
 import model.Proprietaire;
 import model.TypeBien;
 
-//Necessaire pour mettre en place le setUp et le cleanUp 
 @TestInstance(Lifecycle.PER_CLASS)
 public class BienTest {
+  private BienDAO bienDAO;
+  private Proprietaire P;
+  private BienImmobilier bien;
+  private String id;
 
-    private BienDAO bienDAO;
-    private Proprietaire P;
-    private BienImmobilier bien;
-    private String id;
+  @BeforeAll
+  public void setUp() {
+    bienDAO = new BienDAO();
+    P = new Proprietaire(1, "Voisin", "Clément", "clembs@clembs.com", "");
+    bien = new BienImmobilier("BienTest", TypeBien.BATIMENT, "11 rue des tulipes", "145 étage 3", "31400",
+        "Toulouse");
+    id = bien.getId();
+  }
 
-    @BeforeAll
-    public void setUpTest() {
-        bienDAO = new BienDAO();
-        P = new Proprietaire(1, "Voisin", "Clément", "clembs@clembs.com", "");
-        bien = new BienImmobilier("dnwdndn2", TypeBien.BATIMENT, "11 rue des tulipes", " 145 étage 3 ", "31400",
-                "Toulouse");
-        id = bien.getId();
-    }
+  @Test
+  public void testConstructeurAvecValeurCorrecte() {
+    assertNotNull(bien);
+    assertEquals("BienTest", bien.getId());
+    assertEquals(TypeBien.BATIMENT, bien.getTypeBien());
+    assertEquals("11 rue des tulipes", bien.getAdresse());
+    assertEquals("145 étage 3", bien.getComplementAdresse());
+    assertEquals("31400", bien.getCodePostal());
+    assertEquals("Toulouse", bien.getVille());
+  }
 
-    @Test
-    public void testConstructeurAvecValeurCorrecte() {
-        assertNotNull(bien);
-        assertEquals("dnwdndn2", bien.getId());
-        assertEquals(TypeBien.BATIMENT, bien.getTypeBien());
-        assertEquals("11 rue des tulipes", bien.getAdresse());
-        assertEquals(" 145 étage 3 ", bien.getComplementAdresse());
-        assertEquals("31400", bien.getCodePostal());
-        assertEquals("Toulouse", bien.getVille());
-    }
+  @Test
+  public void testConstructeurAvecValeurNull() {
+    BienImmobilier bien2 = new BienImmobilier(
+        null,
+        null,
+        null,
+        null,
+        null,
+        null);
 
-    @Test
-    public void testConstructeurAvecValeurNull() {
-        BienImmobilier bien2 = new BienImmobilier(
-                null,
-                null,
-                null,
-                null,
-                null,
-                null);
+    assertNotNull(bien2);
+    assertNull(bien2.getId());
+    assertNull(bien2.getTypeBien());
+    assertNull(bien2.getAdresse());
+    assertNull(bien2.getComplementAdresse());
+    assertNull(bien2.getCodePostal());
+    assertNull(bien2.getVille());
+  }
 
-        assertNotNull(bien2);
-        assertNull(bien2.getId());
-        assertNull(bien2.getTypeBien());
-        assertNull(bien2.getAdresse());
-        assertNull(bien2.getComplementAdresse());
-        assertNull(bien2.getCodePostal());
-        assertNull(bien2.getVille());
-    }
+  @Test
+  public void testConstructeurAvecStringVide() {
+    BienImmobilier bien3 = new BienImmobilier(
+        "",
+        TypeBien.BATIMENT,
+        "",
+        "",
+        "",
+        "");
 
-    @Test
-    public void testConstructeurAvecStringVide() {
-        BienImmobilier bien3 = new BienImmobilier(
-                "",
-                TypeBien.BATIMENT,
-                "",
-                "",
-                "",
-                "");
+    assertNotNull(bien3);
+    assertEquals("", bien3.getId());
+    assertEquals(TypeBien.BATIMENT, bien3.getTypeBien());
+    assertEquals("", bien3.getAdresse());
+    assertEquals("", bien3.getComplementAdresse());
+    assertEquals("", bien3.getCodePostal());
+    assertEquals("", bien3.getVille());
+  }
 
-        assertNotNull(bien3);
-        assertEquals("", bien3.getId());
-        assertEquals(TypeBien.BATIMENT, bien3.getTypeBien());
-        assertEquals("", bien3.getAdresse());
-        assertEquals("", bien3.getComplementAdresse());
-        assertEquals("", bien3.getCodePostal());
-        assertEquals("", bien3.getVille());
-    }
+  @Test
+  public void testToStringAvecValeurValide() {
+    String expected = "Bâtiment - 11 rue des tulipes, 31400 Toulouse";
+    assertEquals(expected, bien.toString());
+  }
 
-    @Test
-    public void testGetters() {
+  @Test
+  public void testToStringAvecValeurNull() {
+    BienImmobilier bien4 = new BienImmobilier(
+        null,
+        null,
+        null,
+        null,
+        null,
+        null);
 
-        assertEquals("dnwdndn2", bien.getId());
-        assertEquals(TypeBien.BATIMENT, bien.getTypeBien());
-        assertEquals("11 rue des tulipes", bien.getAdresse());
-        assertEquals(" 145 étage 3 ", bien.getComplementAdresse());
-        assertEquals("31400", bien.getCodePostal());
-        assertEquals("Toulouse", bien.getVille());
-    }
+    String expected = "null - null, null null";
+    assertEquals(expected, bien4.toString());
+  }
 
-    @Test
-    public void testToStringAvecValeurValide() {
+  @Test
+  public void testToStringAvecStringVide() {
+    BienImmobilier bien5 = new BienImmobilier(
+        "",
+        TypeBien.BATIMENT,
+        "",
+        "",
+        "",
+        "");
 
-        String expected = "Bâtiment - 11 rue des tulipes, 31400 Toulouse";
-        assertEquals(expected, bien.toString());
-    }
+    String expected = "Bâtiment - ,  ";
+    assertEquals(expected, bien5.toString());
+  }
 
-    @Test
-    public void testToStringAvecValeurNull() {
-        BienImmobilier bien4 = new BienImmobilier(
-                null,
-                null,
-                null,
-                null,
-                null,
-                null);
+  @Test
+  public void TestInsertionEtRecuperationBien() {
+    bienDAO.create(bien, P);
 
-        String expected = "null - null, null null";
-        assertEquals(expected, bien4.toString());
-    }
+    List<BienImmobilier> listbien = bienDAO.getAllBiens(P);
 
-    @Test
-    public void testToStringAvecStringVide() {
-        BienImmobilier bien5 = new BienImmobilier(
-                "",
-                TypeBien.BATIMENT,
-                "",
-                "",
-                "",
-                "");
+    BienImmobilier bienTrouvé = listbien
+        .stream()
+        .filter(b -> b.getId().equals(id))
+        .findFirst()
+        .orElse(null);
 
-        String expected = "Bâtiment - ,  ";
-        assertEquals(expected, bien5.toString());
-    }
+    assertNotNull(bienTrouvé);
+    assertEquals(id, bienTrouvé.getId());
 
-    @Test
-    public void TestInsertionEtRecuperationBien() {
-        bienDAO.create(bien, P);
+    bienDAO.delete(bien, P);
+  }
 
-        List<BienImmobilier> listbien = bienDAO.getAllBiens(P);
+  @Test
+  public void TestModificationAvecValeurValide() {
+    bienDAO.create(bien, P);
 
-        BienImmobilier bienTrouvé = listbien
-                .stream()
-                .filter(b -> b.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+    BienImmobilier nouveauBien = new BienImmobilier(
+        bien.getId(),
+        bien.getTypeBien(),
+        bien.getAdresse(),
+        bien.getComplementAdresse(),
+        bien.getCodePostal(), "Paris");
 
-        assertNotNull(bienTrouvé);
-        assertEquals(id, bienTrouvé.getId());
-    }
+    bienDAO.update(nouveauBien);
 
-    @Test
-    public void TestModificationAvecValeurValide() {
-        bienDAO.create(bien, P);
+    List<BienImmobilier> listbien = bienDAO.getAllBiens(P);
 
-        BienImmobilier nouveauBien = new BienImmobilier(
-                bien.getId(),
-                bien.getTypeBien(),
-                bien.getAdresse(),
-                bien.getComplementAdresse(),
-                bien.getCodePostal(), "Paris");
+    BienImmobilier bienTrouvé = listbien
+        .stream()
+        .filter(b -> b.getId().equals(id))
+        .findFirst()
+        .orElse(null);
 
-        bienDAO.update(nouveauBien);
+    assertNotNull(bienTrouvé);
+    assertEquals(id, bienTrouvé.getId());
+    assertEquals("Paris", bienTrouvé.getVille());
 
-        List<BienImmobilier> listbien = bienDAO.getAllBiens(P);
-
-        BienImmobilier bienTrouvé = listbien
-                .stream()
-                .filter(b -> b.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-
-        assertNotNull(bienTrouvé);
-        assertEquals(id, bienTrouvé.getId());
-        assertEquals("Paris", bienTrouvé.getVille());
-    }
-
-    @AfterEach
-    public void cleanUpEach() {
-        bienDAO.delete(bien, P);
-    }
+    bienDAO.delete(bien, P);
+  }
 }

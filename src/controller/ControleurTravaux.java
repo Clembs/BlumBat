3a50d9a330f1258c5b6331ac644ba061ab0766b8
Alexandre Valente Rosado <alexandre.valente-rosado@etu.iut-tlse3.fr>
2025-01-13 20:21:46 +0100

    implementation du controleur (pas finie)

--- src/controller/ControleurTravaux.java
+++ src/controller/ControleurTravaux.java
@@ -1,11 +1,15 @@
 package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 import dao.TravauxDAO;
 import model.BienImmobilier;
+import model.FactureTravaux;
 import view.PanelTravaux;
 
 
@@ -20,12 +24,64 @@ public class ControleurTravaux implements ActionListener{
         this.bien = bien;
         this.travauxDAO = new TravauxDAO();
     
+        loadData();
+    }
+
+    private void loadData() {
+        List<FactureTravaux> factures = travauxDAO.getAllFacture(bien);
+        DefaultTableModel tableModel = (DefaultTableModel) view.getTable().getModel();
+        tableModel.setRowCount(0); // Réinitialiser le tableau
+
+        double totalPrix = 0;
+
+        for (FactureTravaux facture : factures) {
+            tableModel.addRow(new Object[]{
+                facture.getDescription(),
+                facture.getEntreprise(),
+                facture.getMontantDevis() + "€",
+                facture.getMontantFacture() + "€"
+            });
+            totalPrix += facture.getMontantFacture();
+        }
+
+        // Mettre à jour le prix total dans la vue
+        view.setPrixTotal(totalPrix + "€");
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton boutonClique = (JButton) e.getSource();
+        String boutonTexte = boutonClique.getText();
+
+        switch (boutonTexte) {
+            case "Ajouter Nouvele Travaux":
+                break;
+            case "Supprimer":
+                supprimerTravaux();
+                break;
+            case "Modifier":
+                break;
+        }
+
         
     }
+
+    private void supprimerTravaux() {
+        int selectedRow = view.getTable().getSelectedRow();
+        if (selectedRow >= 0) {
+            DefaultTableModel tableModel = (DefaultTableModel) view.getTable().getModel();
+            String description = (String) tableModel.getValueAt(selectedRow, 0);
+    
+            FactureTravaux factureToDelete = travauxDAO.getFactureByDescription(description, bien);
+    
+            if (factureToDelete != null) {
+                travauxDAO.delete(factureToDelete);
+                loadData(); // Reload la Data apres supression
+                JOptionPane.showMessageDialog(view, "Travail supprimé avec succès !");
+            }
+        } else {
+            JOptionPane.showMessageDialog(view, "Veuillez sélectionner un travail à supprimer.");
+        }
+    }    
     
 }

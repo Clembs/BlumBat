6a32c1bf6af5071c85f437e61847009c2c725d39
Alexandre Valente Rosado <alexandre.valente-rosado@etu.iut-tlse3.fr>
2025-01-13 11:49:13 +0100

    controleur

--- src/controller/ControleurTravaux.java
+++ src/controller/ControleurTravaux.java
@@ -2,7 +2,23 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import dao.TravauxDAO;
+import model.BienImmobilier;
+import view.PanelTravaux;
+
+
 public class ControleurTravaux implements ActionListener{
+
+    private PanelTravaux view;
+    private TravauxDAO travauxDAO;
+    private BienImmobilier bien;
+
+    public ControleurTravaux(PanelTravaux view, BienImmobilier bien) {
+        this.view = view;
+        this.bien = bien;
+        this.travauxDAO = new TravauxDAO();
+    
+    }
     
     @Override
     public void actionPerformed(ActionEvent e) {

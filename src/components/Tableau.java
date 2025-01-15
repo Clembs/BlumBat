f17573b42fc9594f8ebf195a4a3e5593a3ac8f99
Clément Voisin <clembs@clembs.com>
2025-01-15 13:46:10 +0100

    feat: finition de l'apparence & fonctionnalités du tableau

--- src/components/Tableau.java
+++ src/components/Tableau.java
@@ -1,33 +1,63 @@
 package components;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import java.awt.*;
 
 public class Tableau extends JTable {
+  private DefaultTableModel model;
 
-    public Tableau(Object[][] data, Object[] columnNames) {
-        super(new DefaultTableModel(data, columnNames));
-        StyleTableau();
+  public Tableau(String... columnNames) {
+    super();
+    model = new DefaultTableModel(columnNames, 0);
+    this.setModel(model);
+
+    this.setFont(Layout.POLICE_PARAGRAPHES);
+    this.setBackground(Layout.COULEUR_FOND);
+
+    this.setGridColor(Layout.COULEUR_SECONDAIRE);
+    this.setSelectionBackground(Layout.COULEUR_PRIMAIRE_VARIANTE);
+    this.setSelectionForeground(Layout.COULEUR_FOND);
+    this.setRowHeight(40);
+
+    // Non-éditable
+    this.setDefaultEditor(Object.class, null);
+    // Largeur auto des colonnes
+    this.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+
+    // Alignement centré des cellules
+    DefaultTableCellRenderer customCellRenderer = new DefaultTableCellRenderer();
+    customCellRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+
+    for (int i = 0; i < getColumnCount(); i++) {
+      this.getColumnModel().getColumn(i).setCellRenderer(customCellRenderer);
     }
 
-    private void StyleTableau() {
-        setBackground(Layout.COULEUR_FOND);
-        setGridColor(Layout.COULEUR_SECONDAIRE);
-        setSelectionBackground(Layout.COULEUR_PRIMAIRE);
-        setSelectionForeground(Layout.COULEUR_FOND);
-        setFont(Layout.POLICE_PARAGRAPHES);
+    this.getTableHeader().setFont(Layout.POLICE_PETIT_TITRE);
+    this.getTableHeader().setForeground(Layout.COULEUR_FOND);
+    this.getTableHeader().setBackground(Layout.COULEUR_PRIMAIRE);
+    this.getTableHeader().setBorder(new LineBorder(Layout.COULEUR_PRIMAIRE_VARIANTE));
+  }
 
-        DefaultTableCellRenderer cellulecentrer = new DefaultTableCellRenderer();
-        cellulecentrer.setHorizontalAlignment(SwingConstants.CENTER);
-        for (int i = 0; i < getColumnCount(); i++) {
-            getColumnModel().getColumn(i).setCellRenderer(cellulecentrer);
-        }
-        getTableHeader().setFont(Layout.POLICE_ERREUR_SUCCES);
-        getTableHeader().setForeground(Layout.COULEUR_FOND);
-        getTableHeader().setBackground(Layout.COULEUR_INFO);
-        setRowHeight(30);
-        setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-    }
+  public DefaultTableModel getModel() {
+    return this.model;
+  }
+
+  public void setModel(DefaultTableModel model) {
+    this.model = model;
+    super.setModel(model);
+  }
+
+  public void addRow(String... columns) {
+    this.model.addRow(columns);
+  }
+
+  public void removeRow(int rowIndex) {
+    this.model.removeRow(rowIndex);
+  }
+
+  public void clear() {
+    this.model.setRowCount(0);
+  }
 }
